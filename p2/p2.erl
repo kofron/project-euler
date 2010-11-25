b40710224db80fecc8c9857ad366f735d4e50647
Jared Kofron <jared.kofron@gmail.com>
2010-11-24 18:41:12 -0800

    added erlang solution for p2

---
+++ p2/p2.erl
@@ -0,0 +1,23 @@
+% p2.erl
+% solves project euler problem 2
+-module(p2).
+-export([solve/0]).
+
+solve() ->
+    F = gen_fibs(),
+    sum(F).
+
+sum(L) ->
+    sum(L,0).
+sum([], Acc) ->
+    Acc;
+sum([H|T],Acc) ->
+    sum(T,Acc + H).
+
+gen_fibs() ->
+    gen_fibs([2,1]).
+gen_fibs([H1,H2|_T]=L) when (H1 + H2) < 4000000 ->
+    gen_fibs([H1+H2] ++ L);
+gen_fibs(L) ->
+    lists:filter(fun(X) ->
+			 X rem 2 == 0 end, L).
